1b149351d5a558a1a2b5fe405cbbe1e45f46e0ca
Lina María Gallardo Corrales <linamgallardoc3@gmail.com>
2026-07-02 12:50:24 -0500

    figuras

--- src/main/java/com/example/project/ShapeHierarchy.java
+++ src/main/java/com/example/project/ShapeHierarchy.java
@@ -5,5 +5,17 @@ import java.util.ArrayList;
 public class ShapeHierarchy {
     public static void main(String[] args) {
         // TODO: create a list/array of shapes and print each area (polymorphism)
+        ArrayList<Shape> shapes = new ArrayList<>();
+        shapes.add( new Circle(1));
+        shapes.add( new Rectangle(2,3));
+        shapes.add( new Triangle(3, 4));
+
+        for (int i =0; i<shapes.size();i++){
+            System.out.println("El area es: "+shapes.get(i).getArea());
+        }
+        System.out.println("--------Desde for each--------");
+        for (Shape  sape : shapes){
+            System.out.println("El area: "+sape.getArea());
+        }
     }
 }
